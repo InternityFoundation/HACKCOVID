0080e89b690585d7293a9adc3d653be12af2fa3b
Paras Nagpal <paras.nag@gmail.com>
2020-04-12 03:58:53 +0530

    Commit No 6

--- js/main.js
+++ js/main.js
@@ -56,6 +56,8 @@ jQuery(document).ready(function($) {
 	// $(".loader").delay(1000).fadeOut("slow");
  //  $("#overlayer").delay(1000).fadeOut("slow");	
 
+
+
 	var siteMenuClone = function() {
 
 		$('.js-clone-nav').each(function() {
